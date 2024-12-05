287455036229e1c8d6f8566e2ad7e68aa7591ec5
Andrus Diaz German <andrusdiazaleman@gmail.com>
2024-12-05 16:26:52 -0400

    feat: refactor mysql storage

--- src/adapters/storage/mysql/mysql.ts
+++ src/adapters/storage/mysql/mysql.ts
@@ -1,5 +1,5 @@
 import { Config } from "../../../ports/config";
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 
 export class MysqlStorage {
   private _conn: mysql.Connection;
@@ -10,29 +10,16 @@ export class MysqlStorage {
   public conn() {
     return this._conn;
   }
-  
-
 
   constructor(private config: Config) {}
 
-  Connect(): Promise<void> {
-    return new Promise((resolve, reject) => {
-      // create connection
-      const conn = mysql.createConnection({
-        host: this.config.databaseHost(),
-        user: this.config.databaseUser(),
-        password: this.config.databasePassword(),
-        database:this.config.databaseName(),
-      });
-      this._conn = conn;
-
-      // complete connection
-      this._conn.connect((err) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve();
-      });
+  async Connect(): Promise<void> {
+    const conn = await mysql.createConnection({
+      host: this.config.databaseHost(),
+      user: this.config.databaseUser(),
+      password: this.config.databasePassword(),
+      database: this.config.databaseName(),
     });
+    this._conn = conn;
   }
 }
--- src/adapters/storage/mysql/repository/recordData.ts
+++ src/adapters/storage/mysql/repository/recordData.ts
@@ -5,35 +5,17 @@ export class RecordDataMysqlRepository {
   constructor(private mysql: MysqlStorage) {}
 
   async GetAll(): Promise<RecordData[]> {
-    return new Promise((resolve, reject) => {
-      this.mysql
-        .conn()
-        .query(
-          `SELECT id,nombre,idPadre FROM RecordsData`,
-          (err, result, fields) => {
-            if (err) return reject(err);
-
-
-            
-
-            const resultParse = (result as any[]).map(
-              (data) => new RecordData(data.id, data.nombre, data.idPadre)
-            );
-
-            resolve(resultParse);
-          }
-        );
-    });
+    const response = await this.mysql
+      .conn()
+      .query(`SELECT id,nombre,idPadre FROM RecordsData`);
+    return (response[0] as any[]).map(
+      (data) => new RecordData(data.id, data.nombre, data.idPadre)
+    );
   }
 
   async Save(recordData: RecordData): Promise<void> {
-
-    return new Promise((resolve,reject) => {
-      this.mysql.conn().query(`INSERT INTO somosproper.RecordsData 
-        (nombre,idPadre) VALUES ('${recordData.nombre}', ${recordData.idPadre});`,(err =>{
-          if (err) return reject(err);
-          resolve();
-        }))
-    })
+    await this.mysql.conn().query(`INSERT INTO somosproper.RecordsData 
+      (nombre,idPadre) VALUES ('${recordData.nombre}', ${recordData.idPadre});`);
+    return;
   }
 }
